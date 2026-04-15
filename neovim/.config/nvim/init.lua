2189a7b7feaf2b5cfc4d1ab2655ab4f195104655
haze <haze@otherside.sh>
2026-04-14 17:46:45 -0700

    [nvim] cleanup

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,40 +6,37 @@ vim.g.have_nerd_font = true
 -- options
 vim.o.number = true
 vim.o.relativenumber = true
-
 vim.o.mouse = 'a'
-
 vim.o.showmode = false
-
 vim.o.breakindent = true
-
 vim.o.undofile = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 vim.o.signcolumn = 'yes'
-
 vim.o.updatetime = 250
-
 vim.o.timeoutlen = 300
-
 vim.o.splitright = true
 vim.o.splitbelow = true
-
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 vim.o.inccommand = 'split'
-
 vim.o.cursorline = true
-
 vim.o.scrolloff = 10
-
 vim.o.confirm = true
 
 -- keybinds
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- make window nav easier
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- diagnostic keymaps + config
 -- See :help vim.diagnostic.Opts
@@ -54,19 +51,6 @@ vim.diagnostic.config {
 }
 vim.keymap.set('n', '<leader>ql', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix [L]ist' })
 vim.keymap.set('n', '<leader>qf', vim.diagnostic.open_float, { desc = 'Open [Q]uickfix [F]loat' })
-
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- make window nav easier
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- autocommands
 vim.api.nvim_create_autocmd('TextYankPost', {
