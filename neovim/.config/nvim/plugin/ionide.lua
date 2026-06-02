5be3d8a2c7abb3743f8a280ad8af7911ee254dbd
haze <haze@otherside.sh>
2026-06-01 20:01:53 -0700

    [nvim] ionide settings

--- neovim/.config/nvim/plugin/ionide.lua
+++ neovim/.config/nvim/plugin/ionide.lua
@@ -1,5 +1,7 @@
 vim.g['fsharp#backend'] = 'disable'
+vim.g['fsharp#lsp_auto_setup'] = 0
 vim.g['fsharp#fsi_keymap'] = 'custom'
+vim.g['fsharp#lsp_codelens'] = 0
 vim.g['fsharp#fsi_keymap_send'] = '<leader>x'
 vim.g['fsharp#fsi_keymap_toggle'] = '<leader>e'
 vim.pack.add { 'https://github.com/ionide/Ionide-vim' }
