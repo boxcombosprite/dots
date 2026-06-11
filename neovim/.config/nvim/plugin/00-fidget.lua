ef9b0f8b756be2f01eb72d17fa234497b8d6c58b
haze <haze@otherside.sh>
2026-06-10 20:58:02 -0700

    [nvim] make fidget less intrusive

--- neovim/.config/nvim/plugin/00-fidget.lua
+++ neovim/.config/nvim/plugin/00-fidget.lua
@@ -1,2 +1,11 @@
 vim.pack.add { 'https://github.com/j-hui/fidget.nvim' }
-require('fidget').setup {}
+require('fidget').setup {
+  progress = {
+    suppress_on_insert = true,
+    ignore_done_already = true,
+    display = {
+      render_limit = 12,
+      done_ttl = 2,
+    },
+  },
+}
